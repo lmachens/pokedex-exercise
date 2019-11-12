6c9cb5f87bc84a9394ff90640a7230848f95ccda
lmachens <leon.machens@googlemail.com>
2019-11-12 10:42:14 +0100

    listen to input event

--- src/index.js
+++ src/index.js
@@ -9,16 +9,8 @@ resetInput(searchInput);
 setChild(resultsElement, createNoPokemons());
 
 // Add event listeners
-
-/**
- * Find the correct event to listen for input changes.
- */
-searchInput.addEventListener('REPLACE_ME', event => {
-  /**
-   * You can verify that this event is fired in the Browser console.
-   * Can you find the value of searchInput in this event?
-   */
-  console.log('Great! This event is fired:', event);
+searchInput.addEventListener('input', event => {
+  console.log('Great! This event is fired:', event.target.value);
 
   /**
    * Search for your pokemons now, create elements and add them to your results.
