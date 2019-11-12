ce4b1db7663f3becbc980f4fee514da416d8a2ee
golubs <32750555+golubs@users.noreply.github.com>
2019-11-12 11:39:56 +0100

    listen to input event (#1)

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ setChild(resultsElement, createNoPokemons());
 /**
  * Find the correct event to listen for input changes.
  */
-searchInput.addEventListener('REPLACE_ME', event => {
+searchInput.addEventListener('input', event => {
   /**
    * You can verify that this event is fired in the Browser console.
    * Can you find the value of searchInput in this event?
